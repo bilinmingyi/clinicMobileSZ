b9d0b9915682c54316d48ca438e4969b8a839095
liangboliang <boliangleung@163.com>
2019-03-28 12:47:04 +0800

    加了reset重置样式和styl-loadere依赖和之前项目的font字体包

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,12 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style >
+@import "./assets/css/reset.css";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  background: red;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
